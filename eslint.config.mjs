a32a7d6ec86758f83723bb34888ccbfee27c9e35
Manish Dahiya <manishdahiya0944@gmail.com>
2024-12-30 23:01:21 +0530

    add env

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,6 @@ export default tseslint.config(
       },
     },
     rules: {
-      "no-console": "error",
     }
   },
 
---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+import { config } from "dotenv";
+config();
+
+const { PORT, NODE_ENV } = process.env;
+
+export const CONFIG = { PORT, NODE_ENV };
